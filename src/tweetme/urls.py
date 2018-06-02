dfe213ced94e623284b70aae160f489845d92853
bzdvdn <bzdv.dn@gmail.com>
2018-06-02 14:30:23 +0300

    some styling html for list view

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -23,7 +23,7 @@ from .views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^$', home, name='home'),
     url(r'^tweet/', include('tweets.urls', namespace='tweet')),
 ]
 
