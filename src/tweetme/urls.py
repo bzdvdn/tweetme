876be969c50348a69f6c8ce2e8874ea1fbc7ae63
bzdvdn <bzdv.dn@gmail.com>
2018-06-03 13:18:19 +0300

    create following functionality

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='home'),
     url(r'^tweet/', include('tweets.urls', namespace='tweet')),
     url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
-    url(r'^', include('accounts.urls', namespace='profiles')),
+    url(r'^profiles/', include('accounts.urls', namespace='profiles')),
 ]
 
 if settings.DEBUG:
