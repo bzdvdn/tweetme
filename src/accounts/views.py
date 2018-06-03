04995727ece633adcbeb2ebf5fdeda8aae21dd70
bzdvdn <bzdv.dn@gmail.com>
2018-06-03 10:34:22 +0300

    add user detail view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your views here.
+
+class UserDetailView(DetailView):
+	template_name 	= 'accounts/user_detail.html'
+	queryset 		= User.objects.all()
+	# lookup_field 	= 'username'
+
+	# def get_slug_field():
+	# 	return username
+
+	def get_object(self):
+		return get_object_or_404(
+				User,
+				username__iexact=self.kwargs.get("username")
+				)
