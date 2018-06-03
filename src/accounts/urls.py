00c93129252df68d5dcde48c20044b08a6a914cf
bzdvdn <bzdv.dn@gmail.com>
2018-06-03 16:07:00 +0300

    follow/unfollow functionallity

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
 from django.views.generic.base import RedirectView 
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
 
 urlpatterns = [
 	# url(r'^create/$', TweetCreateView.as_view(), name='create'),
 	# url(r'^search/$', TweetListView.as_view(), name="list"),
+	url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name="follow"),
 	url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name="detail"),
 	# url(r'^(?P<id>\d+)/update/$', TweetUpdateView.as_view(), name="update"),
 	# url(r'^(?P<id>\d+)/delete/$', TweetDeleteView.as_view(), name="delete"),
