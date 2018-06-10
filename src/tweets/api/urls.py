427cd9b9251bff2c406a5dee70f9b9f975669fa1
bzdvdn <bzdv.dn@gmail.com>
2018-06-10 15:25:39 +0300

    js retweet functionallity

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from .views import (
 	TweetCreateAPIView,
-	TweetListAPIView
+	TweetListAPIView,
+	RetweetAPIView,
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'), #/api/tweet/
 	url(r'create/$', TweetCreateAPIView.as_view(), name='create'), #/api/tweet/
+	url(r'^(?P<tweet_id>\d+)/retweet/$', RetweetAPIView.as_view(), name="retweet"),
 ]
